ba57f457c37cb47132dc575149a4ed25c40a9d83
Lourdes <lourdeswairimu@gmail.com>
2024-05-17 13:04:37 +0300

    error solved

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -51,7 +51,7 @@
             <span class="text-center font-bold">Or</span>
             <div class="w-3/5 mx-auto mt-4">
                 <a href="{{ route('google-auth')}}"
-                class="focus:ring-2 focus:ring-indigo-800 focus:ring-offset-4 border-black-300 flex items-center px-4 py-2 rounded-1g bg-black shadow-md hover:shadow-lg">
+                class="focus:ring-2 focus:ring-indigo-800 focus:ring-offset-4 border-black-300 flex items-center px-4 py-2 rounded-1g bg-black shadow-md hover:shadow-2xl">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" 
                         class="bi bi-google" viewBox="0 0 16 16">
                         <path d="M15.545 6.558a9.4 9.4 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.7 7.7 0 0 1 5.352 2.082l-2.284 2.284A4.35 4.35 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.8 4.8 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.7 3.7 0 0 0 1.599-2.431H8v-3.08z"/>
